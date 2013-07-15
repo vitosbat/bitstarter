5b91dd192a04fa6e339ef5bfecaedd0421bbf033
Viktor Savitskiy <vitosbat@gmail.com>
2013-07-15 04:24:58 +0000

    Add buffers

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+  var buff = new Buffer(fs.readFileSync('index.html', 'utf-8'));
+  response.send(buff.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
